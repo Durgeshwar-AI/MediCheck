4e134d4ad905f7e89e62d69df575211a677378c6
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2025-02-28 14:26:19 +0530

    Login and Register page initial commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,6 +4,8 @@ import { Routes,Route } from "react-router-dom";
 import Contact from "./Pages/Contact";
 import Team from "./Pages/Team";
 import Support from "./Pages/Support";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/team" element={<Team/>} />
         <Route path="/support" element={<Support/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </div>
   );
