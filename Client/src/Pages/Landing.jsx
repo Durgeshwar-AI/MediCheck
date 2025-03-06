9a88c45c9a6d942e718896d55be66fd29780b12c
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2025-03-06 07:40:37 +0530

    Hero added to landing page

--- Client/src/Pages/Landing.jsx
+++ Client/src/Pages/Landing.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../Components/Hero'
+import Footer from '../Components/Footer'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <>
+      <Hero/>
+      <Footer/>
+    </>
   )
 }
 
