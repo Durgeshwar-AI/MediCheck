d8ea050a39dd858a366159f5d7ef8abb3790f8b5
Manab Biswas <manabbiswas108108+github@gmail.com>
2025-03-13 09:17:04 +0530

    Add About component and integrate it into the Landing page; update Footer and FooterInfo links

--- Client/src/Pages/Landing.jsx
+++ Client/src/Pages/Landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import Footer from '../Components/Footer'
+import About from '../Components/About'
 
 const Landing = () => {
   return (
     <>
       <Hero/>
+      <About/>
       <Footer/>
     </>
   )
