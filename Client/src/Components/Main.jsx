c201712e854f0457f25f278344ef1801202bebf3
soura1334 <sourajeet1334@gmail.com>
2025-03-17 12:49:53 +0530

    made the homepage responsive

--- Client/src/Components/Main.jsx
+++ Client/src/Components/Main.jsx
@@ -19,7 +19,7 @@ const metriclst = [
   },
   {
     img: "",
-    data: "8547",
+    data: "8,547",
     name: "Steps Today",
   },
   {
@@ -68,7 +68,7 @@ function Main() {
       <span className="m-10 p-2 border border-dashed rounded-full text-center">
         Smartwatch Connected
       </span>
-      <div className="m-5 grid grid-cols-5 gap-3">
+      <div className="m-5 sm:grid sm:grid-cols-5 gap-3">
         <Metrics />
         <Sidebar />
       </div>
@@ -80,9 +80,9 @@ function Metrics() {
   const metrics1 = metriclst;
 
   return (
-    <div className="col-span-3 border rounded-md">
+    <div className="sm:col-span-3 border rounded-md">
       <MenuTop data= {"Current Health Metrics"} />
-      <div className="grid grid-cols-4 grid-rows-2 gap-6 p-6">
+      <div className="grid sm:grid-cols-4 sm:grid-rows-2 grid-cols-2 grid-rows-3  gap-6 p-6">
         {metrics1.map((metric) => (
           <MetricComponent metricObj={metric} key={metric.name} />
         ))}
@@ -97,15 +97,17 @@ function Sidebar() {
     const records = recordlst;
 
   return (
-    <div className="border rounded-md col-span-2">
+    <div className=" sm:col-span-2 sm:my-0 my-4 ">
+      <div className="border rounded-md mb-4">
       <MenuTop data={"Upcoming Appointments"} />
-      <div>
+      <div className="">
         {apps.map((app)=>
             <Appointment appObj={app} key={app.name}/> )}
-      </div>
+      </div></div>
+      <div className="border rounded-md">
       <MenuTop data={"Recent Medical History"}/>
       {records.map((rec)=> ( <Record recObj={rec} key={rec.name} /> ))}
-    </div>
+    </div></div>
   );
 }
 
@@ -118,7 +120,7 @@ MetricComponent.propTypes = {
 
 function MetricComponent({ metricObj }) {
   return (
-    <div className="border rounded-lg text-center p-5 px-10 flex flex-col items-center justify-between h-50">
+    <div className="border rounded-lg text-center p-5 px-10 flex flex-col items-center justify-between sm:h-50">
       <div className="border-2 rounded-full bg-orange-200 w-16 h-16"></div>
       <p>{metricObj.data}</p>
       <p>{metricObj.name}</p>
@@ -158,7 +160,7 @@ Record.propTypes = {
 function Record({recObj}){
     return (
         <div className="border rounded-md m-4 p-4 px-7 flex justify-between">
-            <p className="text-lg">{recObj.name}</p>
+            <p className="sm:text-lg">{recObj.name}</p>
             <p>{recObj.date}</p>
         </div>
     )
@@ -167,12 +169,12 @@ function Record({recObj}){
 function Appointment({appObj}){
 
     return (
-        <div className="grid grid-cols-5 p-4 m-4 border rounded-md">
+        <div className="grid sm:grid-cols-5 grid-cols-3 p-4 m-4 border rounded-md">
             <div className="border rounded-sm w-15 h-15 p-2 grid grid-rows-2 items-center justify-center text-center">
             <p>{appObj.date}</p>
             <p>{appObj.month}</p>
             </div>
-            <div className="col-span-4">
+            <div className="sm:col-span-4 col-span-2">
                 <p className="font-bold text-lg mb-2">{appObj.name}</p>
                 <p>{appObj.time} - {appObj.location}</p>
             </div>
